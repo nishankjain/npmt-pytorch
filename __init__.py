b3439a4324b12ac330ee1ed915a65a146b4f5ec5
Sarthak Nandi <sarthakn@gmail.com>
2019-04-23 21:24:38 -0400

    Changed hyperparameters

--- __init__.py
+++ __init__.py
@@ -432,7 +432,7 @@ class LSTMDecoder(FairseqIncrementalDecoder):
         x = x.transpose(1, 0)
 
         # srclen x tgtlen x bsz -> bsz x tgtlen x srclen
-        if not self.training or self.need_attn:
+        if not self.training and self.need_attn:
             attn_scores = attn_scores.transpose(0, 2)
         else:
             attn_scores = None
@@ -504,20 +504,20 @@ def Linear(in_features, out_features, bias=True, dropout=0):
 
 @register_model_architecture('npmt', 'npmt')
 def base_architecture(args):
-    args.dropout = getattr(args, 'dropout', 0.5)
-    args.encoder_embed_dim = getattr(args, 'encoder_embed_dim', 256)
+    args.dropout = getattr(args, 'dropout', 0.1)
+    args.encoder_embed_dim = getattr(args, 'encoder_embed_dim', 512)
     args.encoder_embed_path = getattr(args, 'encoder_embed_path', None)
     args.encoder_freeze_embed = getattr(args, 'encoder_freeze_embed', False)
     args.encoder_hidden_size = getattr(args, 'encoder_hidden_size', args.encoder_embed_dim)
-    args.encoder_layers = getattr(args, 'encoder_layers', 2)
-    args.encoder_bidirectional = getattr(args, 'encoder_bidirectional', True)
+    args.encoder_layers = getattr(args, 'encoder_layers', 1)
+    args.encoder_bidirectional = getattr(args, 'encoder_bidirectional', False)
     args.encoder_dropout_in = getattr(args, 'encoder_dropout_in', args.dropout)
     args.encoder_dropout_out = getattr(args, 'encoder_dropout_out', args.dropout)
     args.decoder_embed_dim = getattr(args, 'decoder_embed_dim', 512)
     args.decoder_embed_path = getattr(args, 'decoder_embed_path', None)
     args.decoder_freeze_embed = getattr(args, 'decoder_freeze_embed', False)
     args.decoder_hidden_size = getattr(args, 'decoder_hidden_size', args.decoder_embed_dim)
-    args.decoder_layers = getattr(args, 'decoder_layers', 2)
+    args.decoder_layers = getattr(args, 'decoder_layers', 1)
     args.decoder_out_embed_dim = getattr(args, 'decoder_out_embed_dim', 512)
     args.decoder_attention = getattr(args, 'decoder_attention', '1')
     args.decoder_dropout_in = getattr(args, 'decoder_dropout_in', args.dropout)
@@ -529,28 +529,27 @@ def base_architecture(args):
 
 @register_model_architecture('npmt', 'npmt_iwslt_de_en')
 def lstm_wiseman_iwslt_de_en(args):
-    args.dropout = getattr(args, 'dropout', 0.5)
+    args.dropout = getattr(args, 'dropout', 0.1)
     args.encoder_embed_dim = getattr(args, 'encoder_embed_dim', 256)
-    args.encoder_dropout_in = getattr(args, 'encoder_dropout_in', 0.5)
-    args.encoder_dropout_out = getattr(args, 'encoder_dropout_out', 0.5)
-    args.decoder_embed_dim = getattr(args, 'decoder_embed_dim', 512)
-    args.decoder_out_embed_dim = getattr(args, 'decoder_out_embed_dim', 512)
-    args.decoder_dropout_in = getattr(args, 'decoder_dropout_in', 0.5)
+    args.encoder_dropout_in = getattr(args, 'encoder_dropout_in', 0)
+    args.encoder_dropout_out = getattr(args, 'encoder_dropout_out', 0)
+    args.decoder_embed_dim = getattr(args, 'decoder_embed_dim', 256)
+    args.decoder_out_embed_dim = getattr(args, 'decoder_out_embed_dim', 256)
+    args.decoder_dropout_in = getattr(args, 'decoder_dropout_in', 0)
     args.decoder_dropout_out = getattr(args, 'decoder_dropout_out', args.dropout)
-    args.window_size = getattr(args, 'window_size', 7)
     base_architecture(args)
 
 
-# @register_model_architecture('npmt', 'lstm_luong_wmt_en_de')
-# def lstm_luong_wmt_en_de(args):
-#     args.encoder_embed_dim = getattr(args, 'encoder_embed_dim', 1000)
-#     args.encoder_layers = getattr(args, 'encoder_layers', 4)
-#     args.encoder_dropout_out = getattr(args, 'encoder_dropout_out', 0)
-#     args.decoder_embed_dim = getattr(args, 'decoder_embed_dim', 1000)
-#     args.decoder_layers = getattr(args, 'decoder_layers', 4)
-#     args.decoder_out_embed_dim = getattr(args, 'decoder_out_embed_dim', 1000)
-#     args.decoder_dropout_out = getattr(args, 'decoder_dropout_out', 0)
-#     base_architecture(args)
+@register_model_architecture('lstm', 'lstm_luong_wmt_en_de')
+def lstm_luong_wmt_en_de(args):
+    args.encoder_embed_dim = getattr(args, 'encoder_embed_dim', 1000)
+    args.encoder_layers = getattr(args, 'encoder_layers', 4)
+    args.encoder_dropout_out = getattr(args, 'encoder_dropout_out', 0)
+    args.decoder_embed_dim = getattr(args, 'decoder_embed_dim', 1000)
+    args.decoder_layers = getattr(args, 'decoder_layers', 4)
+    args.decoder_out_embed_dim = getattr(args, 'decoder_out_embed_dim', 1000)
+    args.decoder_dropout_out = getattr(args, 'decoder_dropout_out', 0)
+    base_architecture(args)
 
 
 @register_task('load_dataset')
