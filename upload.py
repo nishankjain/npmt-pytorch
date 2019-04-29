f66c03c441866e264641d7a08000e56ba2377554
Sarthak Nandi <sarthakn@gmail.com>
2019-04-29 14:32:25 -0400

    Changed checkpoint ids

--- upload.py
+++ upload.py
@@ -27,8 +27,8 @@ def authenticate():
 def upload_best():
     authenticate()
     if os.path.isfile('./checkpoints/checkpoint_best.pt'):
-        folder_id = '1tjvgBWhm9Quzy1yfnX0AOJZz55N5YZGj' # NPMT folder ID
-        f = drive.CreateFile({"parents": [{"kind": "drive#fileLink", "id": folder_id}], 'title': 'checkpoint_best.pt', 'id': '1MGtk-Kiy-vGyE5aUjgt4RZoP0zObod80'})
+        folder_id = '18wJR_eucm6BS3ks5WPl2XlNa7Dsqmd6t' # NPMT folder ID
+        f = drive.CreateFile({"parents": [{"kind": "drive#fileLink", "id": folder_id}], 'title': 'checkpoint_best.pt', 'id': '1vhIfVAL9sbF-o72fy3iL0bo9qVHlHubp'})
         f.SetContentFile('./checkpoints/checkpoint_best.pt')
         f.Upload()
         print('Uploaded best checkpoint with ID {}'.format(f.get('id')))
@@ -36,8 +36,8 @@ def upload_best():
 def upload_last():
     authenticate()
     if os.path.isfile('./checkpoints/checkpoint_last.pt'):
-        folder_id = '1tjvgBWhm9Quzy1yfnX0AOJZz55N5YZGj' # NPMT folder ID
-        f = drive.CreateFile({"parents": [{"kind": "drive#fileLink", "id": folder_id}], 'title': 'checkpoint_last.pt', 'id': '1VqDatgr2uFRsy8Ei92OjQqFMup1sCLz0'})
+        folder_id = '18wJR_eucm6BS3ks5WPl2XlNa7Dsqmd6t' # NPMT folder ID
+        f = drive.CreateFile({"parents": [{"kind": "drive#fileLink", "id": folder_id}], 'title': 'checkpoint_last.pt', 'id': '1B3h1yP1Qbt983r85MFyqXIiI2jUaw-o4'})
         f.SetContentFile('./checkpoints/checkpoint_last.pt')
         f.Upload()
         print('Uploaded last checkpoint with ID {}'.format(f.get('id')))
